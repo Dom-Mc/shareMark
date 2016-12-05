96c9e256f067e91841a3cc8036318cf99934588f
Dom McKellar <dom.mckellar@gmail.com>
2016-12-04 17:18:41 -0800

    Create BookmarkTag model to join bookmarks and tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161205000253) do
+ActiveRecord::Schema.define(version: 20161205002653) do
+
+  create_table "bookmark_tags", force: :cascade do |t|
+    t.integer  "bookmark_id"
+    t.integer  "tag_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "bookmarks", force: :cascade do |t|
     t.string   "summary"
@@ -20,6 +27,12 @@ ActiveRecord::Schema.define(version: 20161205000253) do
     t.integer  "rating"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
