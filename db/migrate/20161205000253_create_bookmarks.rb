3504d7d7bdc7271350a80fcc337995649343c548
Dom McKellar <dom.mckellar@gmail.com>
2016-12-04 18:04:12 -0800

    Update Bookmark attributes

--- db/migrate/20161205000253_create_bookmarks.rb
+++ db/migrate/20161205000253_create_bookmarks.rb
@@ -1,9 +1,10 @@
 class CreateBookmarks < ActiveRecord::Migration[5.0]
   def change
     create_table :bookmarks do |t|
-      t.string :summary
+      t.string :display_name
       t.text :description
       t.string :source
+      t.integer :source_type
       t.string :url
       t.integer :rating
 
--- db/schema.rb
+++ db/schema.rb
@@ -20,13 +20,14 @@ ActiveRecord::Schema.define(version: 20161205002653) do
   end
 
   create_table "bookmarks", force: :cascade do |t|
-    t.string   "summary"
+    t.string   "display_name"
     t.text     "description"
     t.string   "source"
+    t.integer  "source_type"
     t.string   "url"
     t.integer  "rating"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "tags", force: :cascade do |t|
