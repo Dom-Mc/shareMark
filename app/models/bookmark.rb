7edaa535d61991f57f2b6f94fae9381bdd1c819f
Dom McKellar <dom.mckellar@gmail.com>
2016-12-04 18:13:08 -0800

    Add enum source_type in Bookmark model

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,6 @@
 class Bookmark < ApplicationRecord
   has_many :bookmark_tags
   has_many :tags, through: :bookmark_tags
+
+  enum source_type: { "Post" => 0, "Publication" => 1, "Video" => 2, "Thread" => 3, "Other" => 4}
 end
