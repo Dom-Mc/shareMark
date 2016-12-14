66407d8fddb689483e43aecc0c24545bb69928c1
Dom McKellar <dom.mckellar@gmail.com>
2016-12-14 11:12:05 -0800

    Create a navigation component and template

---
+++ app/assets/javascripts/components/shared/navigation/nav.component.js
@@ -0,0 +1,11 @@
+(function(){
+  'use strict'
+
+  const nav = {
+      templateUrl: 'components/shared/navigation/nav.html'
+  }
+
+  angular
+  .module("shareMark")
+  .component("nav", nav);
+}());
