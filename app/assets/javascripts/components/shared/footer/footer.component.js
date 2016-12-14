01535fe00dacc6e3062561895678cdd469ae381a
Dom McKellar <dom.mckellar@gmail.com>
2016-12-14 11:14:02 -0800

    Create footer component and template

---
+++ app/assets/javascripts/components/shared/footer/footer.component.js
@@ -0,0 +1,11 @@
+(function(){
+  'use strict'
+
+  const footer = {
+      templateUrl: 'components/shared/footer/footer.html'
+  }
+
+  angular
+  .module("shareMark")
+  .component("footer", footer);
+}());
