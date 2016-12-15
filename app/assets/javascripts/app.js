f21bbb8fb2998742c62c9c30f68551729b17c844
Dom McKellar <dom.mckellar@gmail.com>
2016-12-15 00:39:42 -0800

    Set up ui-router, add angular-rails-templates and ngclipboard as dependencies

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,33 @@
+(function(){
+  'use strict';
+
+  angular
+    .module('shareMark', ['ui.router', 'templates', 'ngclipboard'])
+    .config(function($stateProvider, $urlRouterProvider){
+
+      $urlRouterProvider.otherwise('/');
+
+      $stateProvider
+      .state('home', {
+        url: "/",
+        template: "<home></home>"
+      })
+
+      .state('search', {
+          url: '/search',
+          template: '<search></search>'
+      })
+
+      .state('new', {
+        url: '/new',
+        template: '<new></new>'
+      })
+
+      .state('show', {
+        url: '/:id',
+        template: '<show></show>'
+      })
+
+    }]);//end config
+
+}());
