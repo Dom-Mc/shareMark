8a5904fc32a1cb0a70b18a3c00e02836ee041493
Dom McKellar <dom.mckellar@gmail.com>
2016-12-15 01:08:03 -0800

    Move navigation template and component to shared folder and update nav links

---
+++ app/assets/javascripts/shared/navigation/navigation.component.js
@@ -0,0 +1,12 @@
+(function(){
+  'use strict'
+
+  const Navigation = {
+      templateUrl: 'shared/navigation/navigation.html'
+  }
+
+  angular
+    .module("shareMark")
+    .component("navigation", Navigation);
+
+}());
