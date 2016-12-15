85e2b0974161e4599237675773bae7c828242af2
Dom McKellar <dom.mckellar@gmail.com>
2016-12-15 08:59:57 -0800

    Update site routes to only include :index, :create, :show, and :update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'application#index'
 
-  resources :bookmark, only: [:index, :create]
+  resources :bookmarks, only: [:index, :create, :show, :update]
 end
