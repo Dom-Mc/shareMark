9b83289cbe157b824ccf443bf68191e3a3e67e35
Dom McKellar <dom.mckellar@gmail.com>
2016-12-15 09:01:27 -0800

    Create seeds file and include Faker to populate fake bookmarks

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# https://github.com/stympy/faker
+
+[Bookmark, Tag].each do |table|
+  table.delete_all
+end
+
+40.times do
+  Tag.create!(
+    name: Faker::Lorem.word
+  )
+end
+
+20.times do
+  bookmark = Bookmark.create!(
+    display_name: Faker::Lorem.word, # Building rails templates
+    description: Faker::Lorem.paragraph(2, true), # "long string of random text (up to 500 characters) optional"
+    source: Faker::Internet.domain_word, # overstack
+    source_type: Faker::Number.between(1, 3), #enum value ()
+    url: Faker::Internet.domain_name, # www.example.com/post5.html
+    rating: Faker::Number.between(1, 100) # number
+  )
+
+  4.times do
+    bookmark.tags << Tag.find(Faker::Number.between(Tag.first.id, Tag.last.id))
+  end
+
+end
