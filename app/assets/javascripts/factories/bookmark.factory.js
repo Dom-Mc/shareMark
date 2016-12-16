a26fdde4d919309c071d269ad78cd63c87552c06
Dom McKellar <dom.mckellar@gmail.com>
2016-12-16 08:54:20 -0800

    Update success callbacks for BookmarkFactory

--- app/assets/javascripts/factories/bookmark.factory.js
+++ app/assets/javascripts/factories/bookmark.factory.js
@@ -9,7 +9,6 @@
 
   function BookmarkFactory($http){
     return {
-      // NOTE: callable methods
       getBookmarks: getBookmarks,
       getBookmark: getBookmark,
       createBookmark: createBookmark,
@@ -18,50 +17,39 @@
 
     function getBookmark(id){
       return $http.get('bookmarks/' + id)
-                  .then(successResponse)
-                  // .then(function(response){
-                  //   return response.data;
-                  // });
+                  .then(function(response){
+                    return response.data;
+                  });
     }
 
-
     function getBookmarks(){
-      //returns our json file via active model serializer
       return $http.get('/bookmarks')
-                  //ajax returns a promise
-                  .then(successResponse)
-                  .catch(errorResponse);
-                  // debugger
+                  .then(function(response){
+                    return response.data;
+                  })
     }//end getBookmarks
 
     function createBookmark(bookmark){
-      var req = {
+      const request = {
         method: "post",
         url: '/bookmarks',
         headers: {
           'Content-Type': 'application/json'
-          // "Content-Type": "application/json; charset = utf-8;"
         },
         data: {
           bookmark: bookmark
         }
       }
-      return $http(req)
-                .catch(errorResponse);
-    }//end createBookmark
+
+      return $http(request)
+                .then(function(response){
+                  return response.data;
+                })
+    }
 
     function updateBookmark(bookmark){
       debugger
     }//end updateBookmark
-
-
-    function successResponse(response){
-      return response.data;
-    }
-
-    function errorResponse(error){
-      return error.data;
-    }
 
   }//end BookmarkFactory
 
