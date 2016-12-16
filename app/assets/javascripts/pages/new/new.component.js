443a4ae12198f757370e95cf9e2de12aae9f710f
Dom McKellar <dom.mckellar@gmail.com>
2016-12-16 08:37:29 -0800

    Update NewComponent to use NewController

--- app/assets/javascripts/pages/new/new.component.js
+++ app/assets/javascripts/pages/new/new.component.js
@@ -2,16 +2,13 @@
   'use strict'
 
   const New = {
-    templateUrl: 'pages/new/new.html'
-
-    // templateUrl: 'components/bookmarks/bookmark-search.html',
-    // controller: 'NewController'
-    // controllerAs: 'model'
+    templateUrl: 'pages/new/new.html',
+    controller: 'NewController',
+    controllerAs: 'model'
   }
 
   angular
     .module("shareMark")
     .component("new", New);
 
-    
 }());
