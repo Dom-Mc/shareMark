ef0276db92a001e2e7c8da6e31fc599ec21c94d1
Dom McKellar <dom.mckellar@gmail.com>
2016-12-16 08:44:25 -0800

    Update ShowController and show template to respond to new for submissions

--- app/assets/javascripts/pages/show/show.controller.js
+++ app/assets/javascripts/pages/show/show.controller.js
@@ -5,8 +5,25 @@
     .module('shareMark')
     .controller('ShowController', ShowController);
 
-    function ShowController() {
+    function ShowController($stateParams, BookmarkFactory, $state) {
+      debugger
+          const model = this;
+          model.getBookmark = getBookmark;
 
+          function getBookmark(){
+            debugger
+            return BookmarkFactory.getBookmark($stateParams.id)
+                                  .then(function(responseData){
+                                    return model.bookmark = responseData;
+                                  });
+          }//end getBookmark
+
+          model.$onInit = function ($stateParams) {
+
+            debugger
+
+            getBookmark();
+          };
 
   }//end ShowController
 
