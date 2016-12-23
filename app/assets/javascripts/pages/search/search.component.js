909122243c8c2f6976ceb87ee4e908bffe75143e
Dom McKellar <dom.mckellar@gmail.com>
2016-12-22 19:16:11 -0800

    Remove old search files and update search component and template

--- app/assets/javascripts/pages/search/search.component.js
+++ app/assets/javascripts/pages/search/search.component.js
@@ -3,9 +3,45 @@
 
   const Search = {
     templateUrl: 'pages/search/search.html',
-    controller: 'SearchController',
-    controllerAs: 'model'
-  }
+    controllerAs: 'model',
+
+    controller: function(BookmarkFactory, RatingFactory){
+      const model = this;
+      model.getBookmarks = getBookmarks;
+      model.bookmarkSortOrder = "-rating";
+      model.getVote = getVote;
+      model.$onInit = function () {
+        getBookmarks();
+      };
+
+      function getBookmarks(){
+        return BookmarkFactory.getBookmarks()
+                              .then(function(responseData){
+                                return model.bookmarks = responseData;
+                            });
+      }//getBookmarks
+
+      function getVote(value, bookmark){
+        if (!bookmark.hasOwnProperty('previousRating')){
+          bookmark.previousRating = bookmark.rating
+        }
+
+        const newVote = RatingFactory.getVote(value, bookmark);
+
+        return BookmarkFactory.saveBookmark(bookmark)
+        .then(function(response){
+          return response.data;
+        });
+      }//end getVote
+
+      model.addToList = addToList;
+      function addToList(){
+        debugger
+      }
+
+    }//end controller
+
+  }//end Search
 
   angular
     .module("shareMark")
