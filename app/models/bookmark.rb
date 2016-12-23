4838414e143c68af602ff6760d140173e96dfb95
Dom McKellar <dom.mckellar@gmail.com>
2016-12-22 20:07:17 -0800

    Add validations to models and custom accepts_nested_attributes_for method for creating new tags in the Bookmark model

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,7 +3,20 @@ class Bookmark < ApplicationRecord
   has_many :tags, through: :bookmark_tags
 
   # NOTE: possible name changes (post or blog)
-  enum source_type: { "Blog/Post" => 0, "Video" => 1, "Conversation Thread" => 2, "Book/Publication" => 3, "Other" => 4}
+  enum resource_type: { "blog/post" => 0, "video" => 1, "conversation thread" => 2, "book/publication" => 3, "other" => 4}
 
-  validates :display_name, length: { in: 5..75 }
+  validates :display_name, presence: true
+  validates :description, presence: true
+  validates :source, presence: true
+  validates :resource_type, presence: true
+  validates :url, presence: true
+  validates :rating, presence: true
+
+  def tag_attributes=(tag_attributes)
+    tag_attributes[:tag_names].each do |tag_name|
+      tag = Tag.find_or_create_by(name: tag_name.downcase)
+      self.tags << tag
+    end
+  end
+
 end
