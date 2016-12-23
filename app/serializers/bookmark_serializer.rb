4e5bc0bc27137e480e5ca28fe05756d9609b1d64
Dom McKellar <dom.mckellar@gmail.com>
2016-12-22 19:08:10 -0800

    Add model relationships to serializers

--- app/serializers/bookmark_serializer.rb
+++ app/serializers/bookmark_serializer.rb
@@ -1,4 +1,4 @@
 class BookmarkSerializer < ActiveModel::Serializer
-  attributes :id, :description, :display_name, :rating, :source, :source_type, :url
+  attributes :id, :description, :display_name, :rating, :source, :resource_type, :url
   has_many :tags
 end
